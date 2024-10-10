14328ea0f855397e852090d7e0aac7dd0b84f44f
SundB1 <sunehb@bu.edu>
2024-10-10 04:02:40 -0400

    changed port

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def search():
     return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices}) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=3000)
